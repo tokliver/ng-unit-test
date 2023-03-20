dc66f62b5dad57067956256241bbd60f057f4568
Olivier <tokliver@gmail.com>
2023-03-20 19:26:44 +0100

    unit test component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,10 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let el: DebugElement ;
 
   /* beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,16 +22,43 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
   */
- beforeEach(()=>{
+ /*beforeEach(()=>{
     TestBed.configureTestingModule({
       declarations: [HomeComponent]
     }).compileComponents().then(()=>{
       fixture = TestBed.createComponent(HomeComponent);
       component = fixture.componentInstance ;
     })
- })
+ })*/
+ beforeEach(waitForAsync(
+ () =>{
+  TestBed.configureTestingModule({
+    declarations: [HomeComponent]
+  }).compileComponents().then(()=>{
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance ;
+    el = fixture.debugElement ;
+  })
+ }
+ ));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have correct contents', ()=>{
+   let pElement = el.queryAll(By.css('p'));
+   expect(pElement[0].nativeElement.textContent).toBe('home works!')
+
+   let buttonElements = el.queryAll(By.css('.btn'));
+   expect(buttonElements[0].nativeElement.disabled).toBeTrue();
+
+   let imageElement = el.queryAll(By.css('img'));
+   expect(imageElement[0].nativeElement.src).toBe("http://imgsrc.com/123");
+   
+   component.title = "Welcome to Angular Test";
+   fixture.detectChanges();
+   let textElement = el.queryAll(By.css('.title'));
+   expect(textElement[0].nativeElement.textContent).toBe("Welcome to Angular Test");
+  })
 });
